ece265be98b58b024ccc789c6df8261f7d5db38b
Aamir Akhter <99446035+Amir-Raza-Teesside@users.noreply.github.com>
2024-05-13 08:38:28 +0500

    unselected Version of Gender Picture

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var men:ImageView
+    lateinit var women:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var gender:String;
+
+        men = findViewById(R.id.imgman);
+        women = findViewById(R.id.imgwomen);
+
+        men.setOnClickListener(View.OnClickListener {
+
+          men.setImageResource(R.drawable.male_selected);
+            women.setImageResource(R.drawable.female_unselected);
+            gender = "Men";
+            Toast.makeText(this, gender, Toast.LENGTH_LONG).show()
+        })
+
+        women.setOnClickListener(View.OnClickListener {
+            men.setColorFilter(ContextCompat.getColor(this, R.color.black));
+            women.setColorFilter(null)
+            gender = "Women"
+            Toast.makeText(this, gender, Toast.LENGTH_LONG).show()
+        })
     }
 }
